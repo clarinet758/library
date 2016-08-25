ecf15d446ee1e6574a24cbbc02708c438c94f688
clarinet758 <clarinet758@gmail.com>
2016-08-26 08:33:31 +0900

    ./def.cpp

---
+++ def.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    int mod=1000000007;
+    int n,m;
+    scanf("%d %d",&n,&m);
+
+    return 0;
+}
