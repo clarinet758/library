8fd0abc58f48178eaf3a0fa8914baffa49df2505
nyon <clarinet758@gmail.com>
2016-08-27 02:11:54 +0900

    自宅PC分とマージ

--- def.cpp
+++ def.cpp
@@ -6,6 +6,14 @@
 #include<algorithm>
 using namespace std;
 
+/**
+ * vector<int>ar(3);
+ * for(auto&e:ar){
+ *     scanf("%d",&e);
+ * }
+ * sort(ar.begin(),ar.end())
+ * int sum=accumulate(ar.begin(),ar.end(),0);
+ **/
 
 int main(){
     int mod=1000000007;
