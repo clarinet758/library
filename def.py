51d5d6b8579b9ecdedc0af2adc7aae22fe9f0f80
nyon <clarinet758@gmail.com>
2016-10-03 02:33:47 +0900

    パスカルの三角形を追加

--- def.py
+++ def.py
@@ -20,8 +20,13 @@ bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
 def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
 def gcd(a,b): return a if b==0 else gcd(b,a%b)
 def lcm(a,b): return a*b/gcd(a,b)
-def euclid_dis(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
+def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
+def pscl(num,l=[1]):
+    for i in range(num):
+        l = map(lambda x,y:x+y,[0]+l,l+[0])
+    return l
+
 
 n=int(raw_input())
 n,k=map(int,raw_input().split())
