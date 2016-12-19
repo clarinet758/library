67cd21539eda7f10b95bfb93e9d234ac0a4debea
clarinet758 <clarinet758@gmail.com>
2016-12-19 23:37:21 +0900

    gcd lcm追加

--- def.cpp
+++ def.cpp
@@ -6,6 +6,13 @@
 #include<algorithm>
 using namespace std;
 
+//typedef int kz;
+//typedef long kz;
+typedef long long kz;
+
+kz gcd(kz a,kz b) { if(b==0) return a; else gcd(b,a%b); }
+kz lcm(kz a,kz b) { return a*b/gcd(a,b); }
+
 /**
  * vector<int>ar(3);
  * for(auto&e:ar){
@@ -14,6 +21,7 @@ using namespace std;
  * sort(ar.begin(),ar.end())
  * int sum=accumulate(ar.begin(),ar.end(),0);
  **/
+
 
 int main(){
     int mod=1000000007;
