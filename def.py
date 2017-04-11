e49cb5843a289826c68dcc80d45b8173ecef7d27
clarinet758 <clarinet758@gmail.com>
2017-04-12 08:06:54 +0900

    out

--- def.py
+++ def.py
@@ -21,10 +21,10 @@ def gcd(a,b): return a if b==0 else gcd(b,a%b)
 def lcm(a,b): return a*b/gcd(a,b)
 def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
-def pscl(num,l=[1]):
-    for i in range(num):
-        l = map(lambda x,y:x+y,[0]+l,l+[0])
-    return l
+#def pscl(num,l=[1]):
+#    for i in range(num):
+#        l = map(lambda x,y:x+y,[0]+l,l+[0])
+#    return l
 
 # "!/usr/bin/env pypy3
 # pscl 要修正
