cf397d049142d9ce99bc276267bb1c2658d5848a
clarinet758 <clarinet758@gmail.com>
2018-12-24 01:39:25 +0900

    fix_gcd,lcm

--- def.py
+++ def.py
@@ -18,16 +18,12 @@ xy=[(1,0),(-1,0),(0,1),(0,-1)]
 bs=[(-1,-1),(-1,1),(1,1),(1,-1)]
 def niten(a,b): return abs(a-b) if a>=0 and b>=0 else  a+abs(b) if a>=0 else abs(a)+b if b>=0 else abs(abs(a)-abs(b))
 def fib(n): return [(seq.append(seq[i-2] + seq[i-1]), seq[i-2])[1] for seq in [[0, 1]] for i in range(2, n)]
-def gcd(a,b): return a if b==0 else gcd(b,a%b)
-def lcm(a,b): return a*b/gcd(a,b)
+# fractions.gcd(x,y)
+def lcm(a,b): return a*b//fractions.gcd(a,b)
 def eucl(x1,y1,x2,y2): return ((x1-x2)**2+(y1-y2)**2)**0.5
 def choco(xa,ya,xb,yb,xc,yc,xd,yd): return 1 if abs((yb-ya)*(yd-yc)+(xb-xa)*(xd-xc))<1.e-10 else 0
-#def pscl(num,l=[1]):
-#    for i in range(num):
-#        l = map(lambda x,y:x+y,[0]+l,l+[0])
-#    return l
-# fractions.gcd(x,y)
-# pscl 要修正
+
+
 n=int(input())
 n,k=map(int,input().split())
 l=[int(i) for i in input().split()]
