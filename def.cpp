0213d7f23d04d12af501b1be64ed222185086268
clarinet758 <clarinet758@gmail.com>
2020-08-23 22:21:14 +0900

    lf

--- def.cpp
+++ def.cpp
@@ -11,11 +11,10 @@ using namespace std;
 #define sl3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
 #define PI 3.1415926535897932
 
-//typedef long long ll;
 
 
 int lcm(int a,int b) { return a*b/__gcd(a,b); }
-//ll lcm(ll a,ll b) { return a*b/__gcd(a,b); }
+//long long lcm(ll a,ll b) { return a*b/__gcd(a,b); }
 
 /**
  * sort(ar.begin(),ar.end())
