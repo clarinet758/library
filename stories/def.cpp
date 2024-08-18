702e6f439c0d9e3f4b8f74c39327b127e2380aec
clarinet758 <clarinet758@gmail.com>
2024-08-19 02:25:59 +0900

    ooops

---
+++ stories/def.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n)  for(int i=0;i<n;++i)
+#define per(i,n)  for(int i=n-1;i>=0;--i)
+#define sc1(a)  scanf("%d",&a)
+#define sc2(a,b)  scanf("%d %d",&a,&b)
+#define sc3(a,b,c)  scanf("%d %d %d",&a,&b,&c)
+#define sl1(a)  scanf("%lld",&a)
+#define sl2(a,b)  scanf("%lld %lld",&a,&b)
+#define sl3(a,b,c)  scanf("%lld %lld %lld",&a,&b,&c)
+#define PI 3.1415926535897932
+#define print(a) cout << a << endl
+#define pp puts("")
+
+#define Yes printf("Yes\n")
+#define No printf("No\n")
+
+//int64_t はatcoderメリット不明のため long long
+typedef long long ll;
+//#define ll int64_t
+
+//
+int souwa(int a) {return (1+a)*a/2;}
+int lcm(int a,int b) { return a*b/__gcd(a,b); }
+//l l lcm(ll a,ll b) { return a*b/__gcd(a,b); }
+
+//
+double tilt(int x1,int y1,int x2,int y2) {return (1.0*y2-1.0*y1)/(1.0*x2-1.0*x1);}
+double tri(int xa,int ya,int xb,int yb,int xc,int yc) {return (1.0*xa-1.0*xc)*(1.0*yb-1.0*yc)-(1.0*xb-1.0*xc)*(1.0*ya-1.0*yc);}
+bool sankaku(int a,int b,int c) {vector <int> t={a,b,c};sort(t.begin(),t.end()); return t.at(0)+t.at(1)>t.at(2);};
+
+/** sort(ar.begin(),ar.end())
+ * vector<vector<int>> a(5,vector<int>)
+    int sum=accumulate(ar.begin(),ar.end(),0); 
+    do {// do内部で作られた順列に対して必要な処理を行う
+        // cout << w.at(0) << w.at(1) << w.at(2) << endl;
+    } while (next_permutation(w.begin(),w.end()));  //ex. vector <int> w= {1,2,3}; **/
+
+// 何か貼るときはココから下に
+
+int main(){
+    int mod=1e9+7;
+    int n,k,x,y,z,cnt=0,ans=0;
+    bool bl=0;
+    string s,t;
+    cin >> n;
+    //cin >> m;
+    vector<int> a(n);
+    rep(i,n) cin >> a.at(i);
+
+    //print(ans);
+    
+    bool yf=1; string yn="ls";
+    if(yf){// yes no 解答なら yf=1 yn を ls で大文字小文字調節 bl が　1　なら　yes
+        if(yn=="ls") if(bl) cout << "Yes";  else cout << "No";
+        if(yn=="ll") if(bl) cout << "YES";  else cout << "NO";
+        if(yn=="ss") if(bl) cout << "yes";  else cout << "no";
+        pp;
+    }
+
+    return 0;
+}
